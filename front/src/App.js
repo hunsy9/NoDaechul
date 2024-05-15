d410d9c7ea2c8684c4c4028748fba8ac600d26c1
huni00807 <huni00807@gmail.com>
2024-05-15 21:22:36 +0900

    chore: 로그인, 회원가입 코드 폴더 분리

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Login from './Login';
-import Signup from './Signup';
-
-//  <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-
+import Login from './sign/Login';
+import Signup from './sign/Signup';
+import {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
