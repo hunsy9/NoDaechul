afba08b01c9954f0bcf8488dccc247f303da4cb7
hunsy9 <juniper0917@gmail.com>
2024-05-17 02:28:42 +0900

    feat: MyBatis UserMapper 추가

--- backend/src/main/java/com/cloudcomputing/nodaechul/user/domain/repository/mapper/UserMapper.java
+++ backend/src/main/java/com/cloudcomputing/nodaechul/user/domain/repository/mapper/UserMapper.java
@@ -1,4 +1,19 @@
 package com.cloudcomputing.nodaechul.user.domain.repository.mapper;
 
+import com.cloudcomputing.nodaechul.user.domain.model.dto.SignUpRequestDto;
+import com.cloudcomputing.nodaechul.user.domain.model.UserVO;
+import com.cloudcomputing.nodaechul.user.domain.model.enums.UserState;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+    Long createUser(SignUpRequestDto user);
+
+    void deleteUser(Long id);
+
+    int editUser(SignUpRequestDto user);
+
+    int activateUser(Long userId, UserState state);
+
+    UserVO findByEmail(String email);
 }
