14b84400f6fa62ab396eca433c081fecaaffb577
hunsy9 <juniper0917@gmail.com>
2024-05-17 02:26:47 +0900

    feat: User CRUD Repository 로직 추가

--- backend/src/main/java/com/cloudcomputing/nodaechul/user/domain/repository/UserRepository.java
+++ backend/src/main/java/com/cloudcomputing/nodaechul/user/domain/repository/UserRepository.java
@@ -1,4 +1,35 @@
 package com.cloudcomputing.nodaechul.user.domain.repository;
 
+import com.cloudcomputing.nodaechul.user.domain.model.dto.SignUpRequestDto;
+import com.cloudcomputing.nodaechul.user.domain.model.UserVO;
+import com.cloudcomputing.nodaechul.user.domain.repository.mapper.UserMapper;
+import com.cloudcomputing.nodaechul.user.domain.model.enums.UserState;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class UserRepository {
+    private final UserMapper userMapper;
+
+    public Long createUser(SignUpRequestDto signUpRequestDto) {
+        userMapper.createUser(signUpRequestDto);
+        return signUpRequestDto.getId();
+    }
+
+    public void deleteUser(Long userId) {
+        userMapper.deleteUser(userId);
+    }
+
+    public int editUser(SignUpRequestDto signUpRequestDto){
+        return userMapper.editUser(signUpRequestDto);
+    }
+
+    public int activateUser(Long userId) {
+        return userMapper.activateUser(userId, UserState.Active);
+    }
+
+    public UserVO findByEmail(String email) {
+        return userMapper.findByEmail(email);
+    }
 }
