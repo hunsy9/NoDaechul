721d48a58ac511834f6d67a00adcef42be66e538
huni00807 <huni00807@gmail.com>
2024-05-17 23:08:12 +0900

    feat: 로그인 이후 헤더바 숨기기

---
+++ front/src/auth/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- front/src/index.js
+++ front/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './auth/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider> {/* AuthProvider로 App을 감싸줌 */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
