e03664e79f88db7afa6a046ecbdbb2879516e74d
hunsy9 <juniper0917@gmail.com>
2024-06-04 03:40:37 +0900

    feat: 회원가입 시, 유효한 얼굴 사진인지 판단하는 로직 추가

---
+++ backend/src/main/java/com/cloudcomputing/nodaechul/rekognition/service/RekognitionService.java
@@ -0,0 +1,47 @@
+package com.cloudcomputing.nodaechul.rekognition.service;
+
+import com.cloudcomputing.nodaechul.s3.domain.enums.BucketNameEnum;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.*;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RekognitionService {
+
+    private final RekognitionClient rekognitionClient;
+
+    public static final String defaultCollectionId = "MyCollection";
+
+
+    public Boolean checkValidFace(String key){
+        Image image = Image.builder()
+                .s3Object(S3Object
+                        .builder()
+                        .bucket(BucketNameEnum.AVATAR.getBucketName())
+                        .name(key)
+                        .build())
+                .build();
+
+        IndexFacesRequest indexFacesRequest = IndexFacesRequest.builder()
+                .image(image)
+                .qualityFilter(QualityFilter.AUTO)
+                .collectionId(defaultCollectionId)
+                .externalImageId(key)
+                .detectionAttributesWithStrings("DEFAULT")
+                .build();
+
+        IndexFacesResponse indexFacesResult = rekognitionClient.indexFaces(indexFacesRequest);
+        List<FaceRecord> faceRecords = indexFacesResult.faceRecords();
+
+        return faceRecords.size() == 1;
+    }
+
+}
