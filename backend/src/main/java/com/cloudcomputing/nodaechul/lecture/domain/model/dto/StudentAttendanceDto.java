5ab6511e9d2cf974015bedb489b695dd695447ef
J-1ac <tkdwns26@naver.com>
2024-06-06 16:33:35 +0900

    feat: StudentAttendanceDto 수정

--- backend/src/main/java/com/cloudcomputing/nodaechul/lecture/domain/model/dto/StudentAttendanceDto.java
+++ backend/src/main/java/com/cloudcomputing/nodaechul/lecture/domain/model/dto/StudentAttendanceDto.java
@@ -3,11 +3,16 @@ package com.cloudcomputing.nodaechul.lecture.domain.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class StudentAttendanceDto {
+    public StudentAttendanceDto(String student_id, String name, String avatar) {
+        this.student_id = student_id;
+        this.name = name;
+        this.avatar = avatar;
+    }
+
     private Long id;
-    private String student_id;
-    private String name;
-    private String avatar;
+    private final String student_id;
+    private final String name;
+    private final String avatar;
 }
