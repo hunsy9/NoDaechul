bfe9273140e79f90ffce830da43721d86b5d6b69
huni00807 <huni00807@gmail.com>
2024-06-07 16:26:20 +0900

    fix: 데이터에 맞게 datagrid 수정

--- front/src/components/ClassAttendanceTable.js
+++ front/src/components/ClassAttendanceTable.js
@@ -3,7 +3,9 @@ import HostContext from '../Context/HostContext';
 import { DataGrid } from '@mui/x-data-grid';
 import { Box, Typography } from '@mui/material';
 
-const DataTable = (props) => {
+const DataTable = ({
+  attendanceData
+}) => {
 
   
   const columns = [
@@ -18,30 +20,42 @@ const DataTable = (props) => {
       width: 200,
     },
   ];
-  console.log(props.attendances.attendanceMetaData);
-  console.log(props.attendanceData);
-  console.log(props.setAttendanceData);
+  const boundingBoxes = attendanceData.lectureImageBoundingBoxes;
+  const attendance = attendanceData.attendance.attendanceMetaData;
+  const attendanceUser = attendanceData.attendance.attendanceUserRecords;
+
+  const attendanceFormat = attendanceUser.map((user) => {
+    let row = new Object();
+    row.studentID = user.student_id;
+    row.name = user.name;
+    row.status = user.status;
+    row.Accuracy = user.similarity;
+    return row;
+  });
+  console.log(attendanceFormat);
 
   return (
     <Box>
       <Typography component="h1" variant="h3"
-              sx={{
-                color: "#0a0a0a", 
-                fontFamily: 'Inter',
-                fontWeight: 'bold',
-                fontSize: '23px', 
-                marginBottom: '30px',
-                marginTop: '30px',
-                }}>
-              {/* Total {totalCount} Attend {attendCount} Absent {absentCount} */}
+        sx={{
+          color: "#0a0a0a", 
+          fontFamily: 'Inter',
+          fontWeight: 'bold',
+          fontSize: '23px', 
+          marginBottom: '30px',
+          marginTop: '30px',
+          }}>
+        Total {attendance.total_students} 
+        Attend {attendance.attend_students} 
+        Absent {attendance.absent_students}
       </Typography>      
       <div style={{ height: 300, width: '80%'}}>
-        {/* <DataGrid
-          rows={rows}
+        <DataGrid
+          rows={attendanceFormat}
           columns={columns}
           pageSizeOptions={[5, 10]}
           pagination
-        /> */}
+        />
       </div>
     </Box>
   );
