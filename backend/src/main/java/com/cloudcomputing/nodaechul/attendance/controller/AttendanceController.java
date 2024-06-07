ab30402713a7756d4708c08603b281e1f9e45440
hunsy9 <juniper0917@gmail.com>
2024-06-07 21:13:06 +0900

    fix: 출석부 생성 시, 출석부 id를 반환하도록 변경

--- backend/src/main/java/com/cloudcomputing/nodaechul/attendance/controller/AttendanceController.java
+++ backend/src/main/java/com/cloudcomputing/nodaechul/attendance/controller/AttendanceController.java
@@ -5,6 +5,7 @@ import com.cloudcomputing.nodaechul.annotation.LoginRequired;
 import com.cloudcomputing.nodaechul.attendance.domain.model.AttendanceResponseDto;
 import com.cloudcomputing.nodaechul.attendance.domain.model.CreateAttendanceRequestDto;
 import com.cloudcomputing.nodaechul.attendance.domain.model.GetAttendanceRequestDto;
+import com.cloudcomputing.nodaechul.attendance.exception.CreateAttendanceException;
 import com.cloudcomputing.nodaechul.attendance.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,13 +23,15 @@ public class AttendanceController {
 
     @PostMapping
     @AdminRoleRequired
-    public void createAttendance(@RequestPart("createAttendanceDto") CreateAttendanceRequestDto attendanceRequestDto, @RequestPart("lectureImage") MultipartFile mFile) throws IOException {
+    public ResponseEntity<Long> createAttendance(@RequestPart("createAttendanceDto") CreateAttendanceRequestDto attendanceRequestDto, @RequestPart("lectureImage") MultipartFile mFile) throws IOException {
         Long id = attendanceService.createAttendance(attendanceRequestDto);
         try{
             attendanceService.createStudentAttendanceRecord(attendanceRequestDto, mFile, id);
         }catch (Exception e){
             attendanceService.deleteAttendance(id);
+            throw new CreateAttendanceException("출석부 생성 중 오류가 발생하였습니다.");
         }
+        return ResponseEntity.ok().body(id);
     }
 
     @PostMapping("/get")
---
+++ backend/src/main/java/com/cloudcomputing/nodaechul/attendance/exception/CreateAttendanceException.java
@@ -0,0 +1,8 @@
+package com.cloudcomputing.nodaechul.attendance.exception;
+
+public class CreateAttendanceException extends RuntimeException{
+    public CreateAttendanceException(String message) {
+        super(message);
+    }
+
+}
