aa8936736d34291ef3b4c814c3dd15879bee291b
hunsy9 <juniper0917@gmail.com>
2024-06-08 17:17:28 +0900

    feat: 랜딩페이지 애니메이션 적용

--- front/src/Pages/Landing.js
+++ front/src/Pages/Landing.js
@@ -9,15 +9,16 @@ const Landing = () => {
   return (
     <>
     <HeaderAppBar/>
-    <Container maxWidth="md" style={{ textAlign: 'center', marginTop: '50px' }}>
-      <Typography variant="h4" gutterBottom style={{fontWeight:'bold'}}>
-        Prevent proxy attendance in your class
-      </Typography>
-      <ImageSection src={sampleImage} alt="Content Image"/>
-      <Button variant="contained" color="secondary" sx={{borderRadius: 5}} size="large" component={Link} to="/Signup" style={{ marginTop: '20px', textTransform: 'none', fontWeight:'bold' }}>
-        Get Started!
-      </Button>
-    </Container>
+      <Container maxWidth="md" style={{textAlign: 'center', marginTop: '50px'}}>
+        <Typography className="landing-text" variant="h4">
+        Prevent proxy attendance in your class!
+        </Typography>
+        <ImageSection src={sampleImage} alt="Content Image"/>
+        <Button className="pulse" variant="contained" color="secondary" sx={{borderRadius: 5}} size="large"
+                component={Link} to="/Signup" style={{marginTop: '20px', textTransform: 'none', fontWeight: 'bold'}}>
+          Get Started!
+        </Button>
+      </Container>
     </>
   );
 };
--- front/src/components/ImageSection.js
+++ front/src/components/ImageSection.js
@@ -7,9 +7,9 @@ const ImageSection = ({ src, alt }) => {
       display="flex"
       justifyContent="center"
       alignItems="center"
-      style={{ width: '100%', height: '300px', borderRadius:'20px', backgroundColor: '#FBFCFE', marginBottom: '50px', marginTop: '50px', padding: '20px'}}
+      style={{ width: '100%', height: '400px', borderRadius:'20px', backgroundColor: '#FBFCFE', marginBottom: '50px', marginTop: '50px', padding: '20px'}}
     >
-      <img src={src} alt={alt} style={{ maxWidth: '100%', maxHeight: '100%' }}/>
+      <img src={src} alt={alt} className="floating" style={{ maxWidth: '60%', maxHeight: '100%' }}/>
     </Box>
   );
 };
