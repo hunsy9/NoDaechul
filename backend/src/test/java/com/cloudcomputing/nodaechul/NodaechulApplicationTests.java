ce89c742c8f9c8157605ef28c509fcc5e2fa1f95
hunsy9 <juniper0917@gmail.com>
2024-06-09 15:02:27 +0900

    chore: 빌드 시 테스트를 진행하지 않는 것으로 변경

--- backend/src/test/java/com/cloudcomputing/nodaechul/NodaechulApplicationTests.java
+++ backend/src/test/java/com/cloudcomputing/nodaechul/NodaechulApplicationTests.java
@@ -3,7 +3,7 @@ package com.cloudcomputing.nodaechul;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NodaechulApplicationTests {
 
 	@Test
